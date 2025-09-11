af5494bb3b4ca033755ab03e132e7fa3edfea334
Arjun Bamba <arjunbam@usc.edu>
2025-09-10 23:41:20 -0400

    distinct functions + driver method

--- module_3/load_data.py
+++ module_3/load_data.py
@@ -1,5 +1,11 @@
+import json
 import psycopg
 from psycopg import OperationalError
+from datetime import datetime
+
+
+# CONNECTION AND QUERY HELPERS
+# ---------------------------------
 
 # make a connection with your PostgreSQL database:
 def create_connection(db_name, db_user, db_password, db_host, db_port):
@@ -7,7 +13,7 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
     try:
         # use psycopg.connect() to connect to a PostgreSQL server from within your Python application.
         connection = psycopg.connect(
-            database=db_name,
+            dbname=db_name,
             user=db_user,
             password=db_password,
             host=db_host,
@@ -19,17 +25,7 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
     return connection
 
 
-# create a new database (gradcafe) in the PostgreSQL database server
-def create_database(connection, query):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Query executed successfully")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-
-
+# execute a SQL query
 def execute_query(connection, query):
     connection.autocommit = True
     cursor = connection.cursor()
@@ -38,23 +34,35 @@ def execute_query(connection, query):
         print("Query executed successfully")
     except OperationalError as e:
         print(f"The error '{e}' occurred")
+    finally:
+        cursor.close()
 
 
-def load_data():
+# SETUP DB + APPLICANTS TABLE
+# ---------------------------------
+
+# create a new database (gradcafe) in the PostgreSQL database server
+def setup_database(db_user, db_password, db_host, db_port):
     # Create a connection to a PostgreSQL database
     # Make a connection with the default database postgres
     connection = create_connection(
-        # "postgres", "postgres", "abc123", "127.0.0.1", "5432"
-        "postgres", "postgres", "abc123", "localhost", "5432"
+        "postgres", db_user, db_password, db_host, db_port
     )
-
     create_database_query = "CREATE DATABASE gradcafe"
-    create_database(connection, create_database_query)
+    try:
+        execute_query(connection, create_database_query)
+    except Exception as e:
+        # Database probably exists already
+        print(f"Skipping create database: {e}")
+    finally:
+        connection.close()
 
+# connect to gradcafe db to create applicants table
+def setup_table(db_user, db_password, db_host, db_port):
     # Before we execute queries on the gradcafe database, we need to connect to it
     # Establish a connection with the gradcafe database located in the postgres database server
     connection = create_connection(
-        "gradcafe", "postgres", "abc123", "localhost", "5432"
+        "gradcafe", db_user, db_password, db_host, db_port
     )
 
     create_applicants_table = """
@@ -71,10 +79,28 @@ def load_data():
         gre FLOAT,
         gre_v FLOAT,
         gre_aw FLOAT,
-        degree FLOAT,
+        degree TEXT,
         llm_generated_program TEXT,
         llm_generated_university TEXT
     );
     """
     execute_query(connection, create_applicants_table)
+    connection.close()
+
+
+# DRIVER FUNCTION
+def load_data():
+    DB_USER = "postgres"
+    DB_PASSWORD = "abc123"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
+
+    setup_database(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
+    setup_table(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
+
+
+
+if __name__ == "__main__":
+    load_data()
+
     
