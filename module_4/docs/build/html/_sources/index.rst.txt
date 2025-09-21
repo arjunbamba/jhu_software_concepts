82e7547a5e7a873aa2d8a801766003a7d1b9e811
Arjun Bamba <arjunbam@usc.edu>
2025-09-21 17:22:52 -0400

    docs + req txt

---
+++ module_4/docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,17 @@
+.. jhu_module_4 documentation master file, created by
+   sphinx-quickstart on Mon Sep 22 19:04:32 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+jhu_module_4 documentation
+==========================
+
+Welcome to the API reference for the module 4 coursework. The sections below
+are generated directly from the project docstrings using Sphinx autodoc.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents
+
+   api_reference
---
+++ module_4/docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'jhu_module_4'
+copyright = '2025, Arjun Bamba'
+author = 'Arjun Bamba'
+release = '1'
+
+import os
+import sys
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+
+# Ensure ``src`` is importable for autodoc
+src_path = os.path.join(project_root, "src")
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
