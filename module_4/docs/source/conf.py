88bc31088b40cb0475a04c4bfb633bd882859f71
Arjun Bamba <arjunbam@usc.edu>
2025-09-22 22:22:28 -0400

    updated conf ext and regenerated docs

--- module_4/docs/source/conf.py
+++ module_4/docs/source/conf.py
@@ -23,7 +23,7 @@ if src_path not in sys.path:
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -34,5 +34,12 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+try:
+    import sphinx_rtd_theme
+
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except Exception:  # pragma: no cover - fallback when RTD theme not installed
+    html_theme = 'alabaster'
+
 html_static_path = ['_static']
